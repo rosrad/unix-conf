7855d471174ec169a2fbe3bd1f2454b456cecbf6
renbo <justnow.ren@gmail>
2014-06-05 09:54:43 +0900

    add the base struct elisp

---
+++ init.d/el-get-settings.el
@@ -0,0 +1,60 @@
+; set the el-get path, and create it if it doesn't exist
+;; path to where plugins are kept
+
+(setq plugin-path "~/.emacs.d/plugins/")
+(setq elget-path plugin-path)
+(unless (file-exists-p elget-path)
+  (make-directory elget-path))
+; add el-get to the load path, and install it if it doesn't exist
+;(include-plugin "el-get")
+(add-to-list 'load-path "~/.emacs.d/el-get/")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp))
+  )
+
+; packages to install
+(setq 
+ my-packages '(
+               ;auctex
+               auto-complete
+               color-theme-solarized
+               ein
+               magit
+               markdown-mode
+               matlab-mode
+               ;nxhtml
+	       ztree
+               pydoc-info
+               scss-mode
+	       python-mode
+	       pymacs
+	       ropemacs
+               popup
+               jedi
+               ;nyan-mode
+	       flycheck
+	       go-flymake
+	       go-autocomplete
+	       wacspace
+	       revive-plus
+	       yasnippet
+	       multi-term
+               helm
+               helm-descbinds
+	       highlight-current-line
+	       fill-column-indicator
+               ))   
+
+; first enable shallow clone, so we don't need to clone the entire
+; history of every project
+(setq el-get-git-shallow-clone t)
+
+; then intsall!
+(el-get 'sync my-packages)
+
+(provide 'el-get-settings)
+
---
+++ init.el
@@ -0,0 +1,24 @@
+
+(setq de-emacs-init-file
+      (or load-file-name buffer-file-name))
+(setq de-emacs-config-directory
+      (file-name-directory de-emacs-init-file))
+
+(setq de-init-directory
+      (expand-file-name "init.d/" de-emacs-config-directory))
+(defun load-init-files (filelist)
+  (dolist (file filelist)
+    (load (expand-file-name 
+       (concat de-init-directory file ".el")))
+    (message "Loaded initial config file:%s" file)
+    ))
+(load-init-files '("custom-functions"
+		   "general-settings"
+		   "el-get-settings"
+		   ))
+(setq de-settings-directory
+      (expand-file-name "settings.d" de-emacs-config-directory))
+(dolist (file (directory-files de-settings-directory t "\\.el$"))
+  (load file))
+
+(put 'upcase-region 'disabled nil)
