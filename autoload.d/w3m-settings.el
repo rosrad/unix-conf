a8d12e58af4f1579daf7e4cfa28ec81c49dc5425
renbo <justnow.ren@gmail>
2014-06-21 09:40:39 +0900

    add w3m

---
+++ autoload.d/w3m-settings.el
@@ -0,0 +1,12 @@
+;; this is a config file for w3m browser
+
+(require 'w3m-extension)
+(setq w3m-coding-system 'utf-8
+      w3m-file-coding-system 'utf-8
+      w3m-file-name-coding-system 'utf-8
+      w3m-input-coding-system 'utf-8
+      w3m-output-coding-system 'utf-8
+      w3m-terminal-coding-system 'utf-8)
+;; (require 'w3m)
+;; (require 'w3m-type-ahead)
+;; (require 'w3m-session)
